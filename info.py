eda5febb8d8402420279b29a9694c63b6c6bbc8a
Salmankhan198 <khans231280@gmail.com>
2024-10-01 10:10:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,6 +3,6 @@ API_HASH     = "74308f9ff153646019c37806aa49888e"
 BOT_TOKEN    = "7249405687:AAFcDICNGEgfincujvDRRjMgqcQ31MQ_0bY"
 SESSION      = "1BVtsOGgBuzgkxzGJklaaXEkJ6SW4GAH3o4ejDwBNiPcd3ANAUW_tTUDr4Zd1q5vgWZnRHiodTfD7oF5a4RyF9bD1nAZRQoe4RkDAZq2-rqNSYgXFdNkbKUzsivKAIsCIshfKKRt8xGCzYunKLRqVoRyiDKgs-RSEqDNdTcchbuRM0TlDBbHPbUWiNVPBL_gKfayt85PzWBzZ9SVBDDdUXt0rjeliaabiNcL__B8eUYQjKm_PzOjTwXmto5IBamzwwqcWmlvoKbqTfYsRV5AM3ngzFcX8l6Lx2AJGJQiIlzD-TqbnWWcgewNZ0fn-AH65Cch8PMA7vKLpyhHdzh4oQ3zIIw2So_E="
 DATABASE_URI = "mongodb+srv://Sahilkhan32au:Sahilkhan32@cluster0.0otdx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-LOG_CHANNEL  = -1002486884539
+LOG_CHANNEL  = -1002371978109
 ADMIN        = 6036521478
-CHANNEL      = "All_bot_channel"
+CHANNEL      = Updet_Channel_01
